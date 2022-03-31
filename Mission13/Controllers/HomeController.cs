cc203c87f21d5c0eb8329ae3882886fe66b80afb
dchin1088 <d.chin1088@gmail.com>
2022-03-30 23:51:18 -0600

    Add project files.

---
+++ Mission13/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBowlersRepository _repo { get; set; }
+
+        //Constructor
+        public HomeController(IBowlersRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _repo.Bowlers
+                //.Include(x => Team) //Joins another table in database
+                //.FromSqlRaw("SELECT * FROM Bowlers WHERE BowlerCity LIKE 'b%'")
+                .ToList();
+
+            return View(blah);
+        }
+
+    }
+}
